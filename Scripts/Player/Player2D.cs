de4ec40ca5c4d3e99869c6c88b918236ba2afd69
HUCK <fear2002@naver.com>
2018-02-05 22:14:34 +0900

    2018.02.05 1. 플레이어 중력적용 방식 변경  - 필요없는 스크립트 삭제해야함 (PlayerFoot2D, PlayerFoot3D) 2. Terrain_Grass_Floor_Small 2D 스크립트 추가 (2D 콜라이더 적용)

--- Scripts/Player/Player2D.cs
+++ Scripts/Player/Player2D.cs
@@ -9,14 +9,20 @@ public sealed class Player2D : Player
     // 리지드바디
     Rigidbody2D m_rigidbody2D;
 
-    // 캐릭터가 끼이는 현상때문에 살짝 띄우기 위한 변수
-    RaycastHit2D m_hit2D;
-    int m_layerMask;
-    Vector3 m_upPos;
-
     // 캐릭터가 바라보는 방향
     private E_LookDirection2D m_lookDirection;
     public E_LookDirection2D LookDirection { get { return m_lookDirection; } }
+
+    // 땅 체크를 위한 변수들
+    private RaycastHit2D m_hit;
+    private Vector2 m_rayOrigin;
+    private Vector2 m_rayDirection;
+    private Vector2 m_onGroundUpPos;
+    private Vector3 m_onGroundPos;
+    private float m_boundsX;
+    private float m_rayOriginY;
+    private int m_ignoreLayerMask;
+    private float m_rayDistance;
 
     protected override void Awake()
     {
@@ -24,14 +30,64 @@ public sealed class Player2D : Player
         m_rigidbody2D = GetComponent<Rigidbody2D>();
         m_lookDirection = E_LookDirection2D.Right;
 
-        m_layerMask = (-1) - ((1 << 8) | (1 << 11));
-        m_upPos = new Vector3(0, 0.05f, 0);
+        InitCheckGround();
+    }
+    
+    private void InitCheckGround()
+    {
+        m_rayDirection = Vector2.down;
+
+        // 콜라이더 박스 x 크기
+        m_boundsX = GetComponent<Collider2D>().bounds.extents.x;
+
+        // 레이의 시작 y 위치
+        m_rayOriginY = 0.1f;
+
+        // 플레이어를 올릴 땅 위 위치
+        m_onGroundUpPos = new Vector3(0, 0.05f);
+
+        // 플레이어하고 총알을 무시하는 레이어마스크
+        m_ignoreLayerMask = (-1) - ((1 << 8) | (1 << 11));
+
+        // 레이 거리
+        m_rayDistance = 0.16f;
     }
 
     private void Update()
     {
+        CheckGround();
         Move();
         Jump();
+    }
+
+    private void CheckGround()
+    {
+        // 2방향 체크
+        // 2방향 레이를 쏴서 한곳에 무언가라도 닿으면 땅이라고 알리고 2곳 모두 아무것도 안 닿으면 땅이 아니라고 알림
+
+        // 좌
+        m_rayOrigin = transform.position;
+        m_rayOrigin.x -= m_boundsX;
+        m_rayOrigin.y += m_rayOriginY;
+
+        m_hit = Physics2D.Raycast(m_rayOrigin, m_rayDirection, m_rayDistance, m_ignoreLayerMask);
+
+        if (m_hit.collider != null)
+            Manager.IsGrounded = true;
+        else
+            Manager.IsGrounded = false;
+
+        // 우
+        m_rayOrigin = transform.position;
+        m_rayOrigin.x += m_boundsX;
+        m_rayOrigin.y += m_rayOriginY;
+
+        m_hit = Physics2D.Raycast(m_rayOrigin, m_rayDirection, m_rayDistance, m_ignoreLayerMask);
+
+        if (m_hit.collider != null)
+            Manager.IsGrounded = true;
+        else if (!Manager.IsGrounded)
+            Manager.IsGrounded = false;
     }
 
     // 2D 이동
@@ -63,26 +119,31 @@ public sealed class Player2D : Player
         // 이동벡터 구하기
         Vector3 movement = Vector3.right * move;
 
-        // 중력을 사용한다면 중력적용
-        if (Manager.UseGravity)
+        //// 땅이라면 땅위치에서 살짝 띄어줌
+        if (Manager.IsGrounded)
         {
-            // 점프 상태이거나 땅이 아니면 중력 적용
-            if (Manager.IsJumping || !Manager.IsGrounded)
+            // 점프중일경우 점프중이 아니라고 알림
+            if (Manager.IsJumping)
+                Manager.IsJumping = false;
+
+            m_rayOrigin = transform.position;
+            m_rayOrigin.y += m_rayOriginY;
+
+            m_hit = Physics2D.Raycast(m_rayOrigin, m_rayDirection, m_rayDistance, m_ignoreLayerMask);
+
+            if (m_hit.collider != null)
             {
-                float nextVelocity = m_rigidbody2D.velocity.y + Manager.Stat.Gravity * Time.deltaTime;
-                movement.y = nextVelocity;
+                m_onGroundPos = transform.position;
+                m_onGroundPos.y = m_hit.point.y + m_onGroundUpPos.y;
+
+                transform.position = m_onGroundPos;
             }
         }
-
-        // 캐릭터가 끼이는 현상때문에 살짝 띄우기 위한 방법
-        if (!Manager.IsJumping && Manager.IsGrounded)
+        // 땅이 아니라면 중력 적용
+        else
         {
-            m_hit2D = Physics2D.Raycast(transform.position, Vector2.down, m_upPos.y, m_layerMask);
-
-            if (m_hit2D.collider != null)
-            {
-                transform.position = new Vector3(transform.position.x, m_hit2D.point.y, transform.position.z) + m_upPos;
-            }
+            float nextVelocity = m_rigidbody2D.velocity.y + Manager.Stat.Gravity * Time.deltaTime;
+            movement.y = nextVelocity;
         }
 
         // 이동
@@ -92,15 +153,10 @@ public sealed class Player2D : Player
     // 점프
     private void Jump()
     {
-        // 스페이스바를 누르고 땅이고 점프상태가 아닐 때 점프
+        // 스페이스바를 누르고 땅일경우 점프
         if(Input.GetKeyDown(KeyCode.Space) && Manager.IsGrounded && !Manager.IsJumping)
         {
-            // 중력사용을 하지 않을 때 점프를 할 경우 중력사용
-            if (!Manager.UseGravity)
-                Manager.UseGravity = true;
-
             m_rigidbody2D.AddForce(Vector2.up * Manager.Stat.JumpPower);
-            Manager.IsGrounded = false;
             Manager.IsJumping = true;
         }
     }
