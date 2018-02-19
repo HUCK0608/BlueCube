f1c6f28aa734f4cc1845b3bca690f6608fa6cf05
HUCK <fear2002@naver.com>
2018-02-19 20:58:26 +0900

    2018.02.19 1. stage1 씬 제작

--- Scripts/Camera/CameraManager.cs
+++ Scripts/Camera/CameraManager.cs
@@ -67,13 +67,28 @@ public sealed class CameraManager : MonoBehaviour
         if (!GameManager.Instance.PlayerManager.Skill_CV.ViewType.Equals(GameLibrary.Enum_View3D) || GameManager.Instance.PlayerManager.Skill_CV.IsChanging)
             return;
 
+        // 마우스 방향의 월드 방향 구하기
+        Vector3 mouseDirection = GetMouseDirectionToWorld();
+
+        // 이동방향 * 거리
+        Vector3 movePoint = mouseDirection.normalized * m_moveMaxDis;
+
+        // 이동
+        m_centerPoint.localPosition = Vector3.Lerp(m_centerPoint.localPosition, movePoint, m_moveSpeed * Time.deltaTime);
+    }
+
+    // 마우스의 월드방향을 구하는 함수
+    public Vector3 GetMouseDirectionToWorld()
+    {
         // 플레이어3D
         GameObject player3D = GameManager.Instance.PlayerManager.Player3D_GO;
 
-        // 마우스 포인터 위치에서 플레이어의 스크린 좌표값을 빼줌
+        // 마우스 스크린 위치
         Vector2 mousePosition = Input.mousePosition;
+
         // 플레이어를 스크린 좌표로 가져오기
         Vector2 playerScreenPosition = Camera.main.WorldToScreenPoint(player3D.transform.position);
+        // 플레이어가 센터가되기위해 마우스위치에서 플레이어 위치를 빼줌
         Vector2 mousePosInCenter = mousePosition - playerScreenPosition;
 
         // 마우스 x에 대한 위치 계산, 마우스 y가 증가할 때 x방향이 증가 z방향이 감소 (z방향은 마우스 y계산에 맞게하기위해 스크린 y / x 의 값을 곱해줌)
@@ -84,11 +99,7 @@ public sealed class CameraManager : MonoBehaviour
         // 마우스 방향의 월드 방향 구하기
         Vector3 mouseDirection = calcMouseX + calcMouseY;
 
-        // 이동방향 * 거리
-        Vector3 movePoint = mouseDirection.normalized * m_moveMaxDis;
-
-        // 이동
-        m_centerPoint.localPosition = Vector3.Lerp(m_centerPoint.localPosition, movePoint, m_moveSpeed * Time.deltaTime);
+        return mouseDirection;
     }
 
     // 카메라 무빙워크 (쿼터뷰에서 사이드뷰로 이동)
