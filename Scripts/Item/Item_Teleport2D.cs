1d84e66fa737e712da771dc61ead40119820c810
HUCK <fear2002@naver.com>
2018-03-06 01:23:39 +0900

    2018.03.05 1. 2D에서 먹을경우 해당 Z위치로 이동하는 아이템 구현

---
+++ Scripts/Item/Item_Teleport2D.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Item_Teleport2D : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag.Equals(GameLibrary.String_Player))
+        {
+            Vector3 teleportPosition = transform.parent.position;
+            teleportPosition.x = other.transform.position.x;
+            other.transform.position = teleportPosition;
+            transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
