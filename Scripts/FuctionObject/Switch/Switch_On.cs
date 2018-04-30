6b5d94493f24d6a70a78648486d6b977637cb94a
김상혁 <fear2002@naver.com>
2018-04-30 18:26:29 +0900

    2018.04.30 1. Switch_OnOff 기능 삭제 2. Switch_On_Stepping 리소스 변경 및 기능 변경

--- Scripts/FuctionObject/Switch/Switch_On.cs
+++ Scripts/FuctionObject/Switch/Switch_On.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class Switch_On : Switch
 {
-    protected MeshFilter m_meshFilter;
-
-    [SerializeField]
-    protected Mesh m_onMesh;
-
-    protected virtual void Awake()
+    protected override void Awake()
     {
-        m_meshFilter = GetComponentInChildren<MeshFilter>();
+        base.Awake();
     }
 
+    /// <summary>스위치를 킨다</summary>
     public virtual void SwitchOn()
     {
+        if (!m_isButtonMove && !m_isOn)
+            StartCoroutine(MoveButton());
+    }
+
+    // 버튼 이동
+    private IEnumerator MoveButton()
+    {
+        m_isButtonMove = true;
+
+        while(true)
+        {
+            if(!GameLibrary.Bool_IsGameStop(m_worldObject))
+            {
+                m_button.localPosition = Vector3.MoveTowards(m_button.localPosition, m_buttonOnPosition, m_buttonMoveSpeed * Time.deltaTime);
+
+                if (m_button.localPosition.Equals(m_buttonOnPosition))
+                    break;
+            }
+
+            yield return null;
+        }
+
+        m_isButtonMove = false;
         m_isOn = true;
-        m_meshFilter.mesh = m_onMesh;
+        m_buttonMeshFilter.mesh = m_buttonOnMesh;
     }
 }
