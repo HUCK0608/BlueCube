b12ca60302d89e9c04849bdfa4a7261289c7d9f6
김상혁 <fear2002@naver.com>
2018-05-11 19:41:54 +0900

    2018.05.11 1. 유리 쉐이더에 필요한 리소스 추가 2. UI 시스템 제작 중 3. 폭발 이펙트 프리팹 변경

---
+++ Scripts/UI/StoryUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryUI : MonoBehaviour
+{
+    private static StoryUI m_instance;
+    public static StoryUI Instance { get { return m_instance; } }
+
+    private void Awake()
+    {
+        m_instance = this;
+    }
+}
--- Scripts/UI/UIManager.cs
+++ Scripts/UI/UIManager.cs
@@ -6,23 +6,63 @@ using System.Text;
 
 public sealed class UIManager : MonoBehaviour
 {
-    private Text m_playerHpUI;
-    [SerializeField]
-    private string m_drawPlayerHpFirstString;
+    private static UIManager m_instance;
+    public static UIManager Instance { get { return m_instance; } }
 
+    [Header("Player Hp")]
+    [SerializeField]
+    private Text m_playerHpText;
+    [SerializeField]
+    private Color m_playerHpChangeColor;
+    [SerializeField]
+    private float m_playerHpChangeColorDurationTime;
+    private Coroutine m_playerHpTextColorChangeCor;
+    
     private void Awake()
     {
-        m_playerHpUI = transform.Find("PlayerHpUI").GetComponent<Text>();
+        m_instance = this;
     }
 
-    private void Update()
+    private void Start()
     {
-        DrawPlayerHpUI();
+        InitUI();
     }
 
-    // 플레이어 체력 UI를 그려줌
-    private void DrawPlayerHpUI()
+    /// <summary>UI 초기화</summary>
+    private void InitUI()
     {
-        m_playerHpUI.text = m_drawPlayerHpFirstString + PlayerManager.Instance.Stat.Hp.ToString();
+        SetPlayerHpText(PlayerManager.Instance.Stat.Hp);
+    }
+
+    /// <summary>플레이어 체력 텍스트를 변경</summary>
+    public void SetPlayerHpText(int hp)
+    {
+        m_playerHpText.text = hp.ToString();
+
+        if(m_playerHpTextColorChangeCor != null)
+            StopCoroutine(m_playerHpTextColorChangeCor);
+        m_playerHpTextColorChangeCor = StartCoroutine(PlayerHpTextColorChange());
+    }
+
+    /// <summary>플레이어 체력 텍스트의 색을 변경</summary>
+    private IEnumerator PlayerHpTextColorChange()
+    {
+        m_playerHpText.color = m_playerHpChangeColor;
+
+        float addTime = 0f;
+
+        while(true)
+        {
+            addTime += Time.deltaTime;
+
+            if (addTime >= m_playerHpChangeColorDurationTime)
+                break;
+
+            yield return null;
+        }
+
+        m_playerHpText.color = Color.white;
+
+        m_playerHpTextColorChangeCor = null;
     }
 }
