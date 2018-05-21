4c8641b919fd923e258ce198694bf64101bcc221
김상혁 <fear2002@naver.com>
2018-05-21 16:59:01 +0900

    2018.05.21 1. Item_Story, Item_Key 더미 모델링 적용 2. PowerLine 리소스 넣기 및 제작 중

---
+++ Scripts/FuctionObject/Special/PowerLine/Special_PowerLine_Line.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Special_PowerLine_Line : MonoBehaviour
+{
+    [System.Serializable]
+    private struct LineDirection
+    {
+        [SerializeField]
+        private bool m_isUp;
+        [SerializeField]
+        private bool m_isRight;
+        [SerializeField]
+        private bool m_isLeft;
+        [SerializeField]
+        private bool m_isDown;
+    }
+
+    [SerializeField]
+    private LineDirection m_lineDirection;
+
+    private void Update()
+    {
+        //if(!Application.isPlaying)
+    }
+
+    private void ChangeLineDirection()
+    {
+
+    }
+}
