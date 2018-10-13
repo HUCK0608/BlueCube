f9d65cc741e5d67cc4f4fc73835f5746fa01a184
김상혁 <fear2002@naver.com>
2018-10-13 23:17:43 +0900

    2018.10.13 1. 열쇠 습득 이펙트 변경 2. 시점전환 튜토리얼 수정,그에 따른 리소스 추가 및 삭제

---
+++ Resources/Model/UI/Tutorial/Skill/NoShadow.shader
@@ -0,0 +1,36 @@
+﻿Shader "Custom/NoShadow" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+
+		CGPROGRAM
+		#pragma surface surf NoLight noshadow
+
+		sampler2D _MainTex;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		fixed4 _Color;
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			o.Albedo = _Color.rgb;
+			o.Alpha = 1;
+		}
+
+		float4 LightingNoLight(SurfaceOutput o, float3 lightDir, float atten)
+		{
+			float4 finalColor;
+			finalColor.rgb = o.Albedo;
+			finalColor.a = 1;
+
+			return finalColor;
+		}
+
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
--- Scripts/Tutorial/Tutorial_Skill.cs
+++ Scripts/Tutorial/Tutorial_Skill.cs
@@ -1,11 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public sealed class Tutorial_Skill : Tutorial
 {
+    [Header("Mouse")]
+    /// <summary>마우스 이미지</summary>
+    [SerializeField]
+    private Image m_mouseImage;
+
+    /// <summary>마우스 피벗</summary>
+    [SerializeField]
+    private Transform m_mousePivot;
+
+    /// <summary>마우스 스프라이트(0 : 디폴트, 1 : 클릭)</summary>
+    [SerializeField]
+    private Sprite[] m_mouseSprites;
+
+    /// <summary>캔버스</summary>
+    private Canvas m_canvas;
+
+    [Header("Arrow")]
+    /// <summary>화살표 머리</summary>
+    [SerializeField]
+    private Transform m_arrowHead;
+
+    /// <summary>화살표 몸 피벗</summary>
+    [SerializeField]
+    private Transform m_arrowBodyPivot;
+
+    /// <summary>화살표 머리 피벗</summary>
+    [SerializeField]
+    private Transform m_arrowHeadPivot;
+
+    /// <summary>화살표 몸이 최대로 커지는 크기</summary>
+    [SerializeField]
+    private float m_arrowBodyMaxSize;
+
+    /// <summary>화살표가 늘어나는 시간</summary>
+    [SerializeField]
+    private float m_arrowIncreaseSizeTime;
+
+    private Coroutine m_logicCor, m_calcMousePositionCor;
+
     private void Start()
     {
+        m_canvas = GameObject.Find("UI").GetComponent<Canvas>();
+
         gameObject.SetActive(false);
     }
 
@@ -13,15 +55,90 @@ public sealed class Tutorial_Skill : Tutorial
     {
         // 스킬 잠금 해제
         PlayerManager.Instance.Skill.SetSkillLock(false);
+
         gameObject.SetActive(true);
 
+        m_logicCor = StartCoroutine(TutorialLogic());
+        m_calcMousePositionCor = StartCoroutine(CalcMousePoistion());
         StartCoroutine(EndCheck());
     }
 
-    /// <summary>튜토리얼 종료 체크</summary>
+    private IEnumerator TutorialLogic()
+    {
+        float increaseValue = m_arrowBodyMaxSize / m_arrowIncreaseSizeTime;
+
+        Vector3 arrowBodyInitScale = new Vector3(1f, 1f, 0f);
+        Vector3 headPoistionError = new Vector3(0f, 0f, -19f);
+
+        // 마우스 이미지 교체시 딜레이
+        WaitForSeconds delay1 = new WaitForSeconds(0.5f);
+
+        while (true)
+        {
+            m_arrowBodyPivot.localScale = arrowBodyInitScale;
+            SetMouseSpirte(m_mouseSprites[0]);
+
+            m_arrowHead.gameObject.SetActive(false);
+
+            yield return delay1;
+
+            SetMouseSpirte(m_mouseSprites[1]);
+
+            yield return delay1;
+
+            m_arrowHead.gameObject.SetActive(true);
+
+            while (true)
+            {
+                Vector3 arrowBodyScale = m_arrowBodyPivot.localScale;
+
+                if (arrowBodyScale.z >= (m_arrowBodyMaxSize))
+                {
+                    arrowBodyScale.z = 0f;
+                    break;
+                }
+
+                arrowBodyScale.z += increaseValue * Time.deltaTime;
+
+                m_arrowBodyPivot.localScale = arrowBodyScale;
+                m_arrowHead.position = m_arrowHeadPivot.position + headPoistionError;
+
+                yield return null;
+            }
+
+
+            yield return delay1;
+
+            SetMouseSpirte(m_mouseSprites[0]);
+
+            yield return delay1;
+        }
+    }
+
+    /// <summary>마우스 위치 계산 코루틴</summary>
+    private IEnumerator CalcMousePoistion()
+    {
+        while(true)
+        {
+            Vector3 mousePivotScreenPosition = Camera.main.WorldToViewportPoint(m_mousePivot.position);
+
+            mousePivotScreenPosition.x *= m_canvas.pixelRect.width;
+            mousePivotScreenPosition.y *= m_canvas.pixelRect.height;
+
+            m_mouseImage.rectTransform.position = mousePivotScreenPosition;
+
+            yield return null;
+        }
+    }
+
+    /// <summary>마우스 스프라이트 교체</summary>
+    private void SetMouseSpirte(Sprite sprite)
+    {
+        m_mouseImage.sprite = sprite;
+    }
+
     private IEnumerator EndCheck()
     {
-        // 시점변환중일때까지 대기
         yield return new WaitUntil(() => PlayerManager.Instance.IsViewChange);
 
         EndTutorial();
@@ -29,6 +146,8 @@ public sealed class Tutorial_Skill : Tutorial
 
     protected override void EndTutorial()
     {
+        StopCoroutine(m_logicCor);
+        StopCoroutine(m_calcMousePositionCor);
         gameObject.SetActive(false);
     }
 }
