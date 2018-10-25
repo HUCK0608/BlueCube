196408c1acf4737fe538fab57aab6dcc983f3b42
김상혁 <fear2002@naver.com>
2018-10-25 19:01:14 +0900

    2018.10.25 1. 크로니클 빌드

--- Scripts/FuctionObject/Etc/CheckPoint/CheckPoint.cs
+++ Scripts/FuctionObject/Etc/CheckPoint/CheckPoint.cs
@@ -8,15 +8,25 @@ public sealed class CheckPoint : MonoBehaviour
     private Vector3 m_savePosition;
     public Vector3 GetSavePosition { get { return m_savePosition; } }
 
+    private float m_colliderSizeZ;
+
     private void Awake()
     {
         m_savePosition = transform.position;
+
+        m_colliderSizeZ = GetComponentInChildren<BoxCollider>().size.z;
     }
 
     /// <summary>체크포인트 등록</summary>
     public void RegisterCheckPoint()
     {
-        CheckPointManager.Instance.SetCheckPoint(this);
-        gameObject.SetActive(false);
+        float playerPositionZ = PlayerManager.Instance.Player3D_Object.transform.position.z;
+
+        if (playerPositionZ >= transform.position.z - m_colliderSizeZ && playerPositionZ <= transform.position.z + m_colliderSizeZ)
+        {
+            Debug.Log("체크포인트 등록");
+            CheckPointManager.Instance.SetCheckPoint(this);
+            gameObject.SetActive(false);
+        }
     }
 }
